9195e3094da58bc609d9fc15928255d4c19a75b0
Nikola Lakovic <nikola.lakovictech@gmail.com>
2022-10-17 21:43:43 +0200

    Finally figured out how Youtube does some of it's stuff

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const CREATE = document.querySelector('.create')
+const CREATE_TEXT = document.querySelector('.create-text');
+const NOTIFICATIONS = document.querySelector('.notifications');
+const NOTIFICATIONS_TEXT = document.querySelector('.notifications-text');
+
+CREATE.addEventListener('mouseover', () => {
+    CREATE_TEXT.style.visibility = 'visible';
+})
+CREATE.addEventListener('mouseout', () => {
+    CREATE_TEXT.style.visibility = 'hidden';
+})
 
 
-
-
+NOTIFICATIONS.addEventListener('mouseover', () => {
+    NOTIFICATIONS_TEXT.style.visibility = 'visible';
+})
+NOTIFICATIONS.addEventListener('mouseout', () => {
+    NOTIFICATIONS_TEXT.style.visibility = 'hidden';
+})
