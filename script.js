08bf4efdb4cc30c703d72adb022d81b45225f41a
Nikola Lakovic <nikola.lakovictech@gmail.com>
2022-10-23 18:09:53 +0200

    added left side menu.

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-const CREATE = document.querySelector('.create')
-const CREATE_TEXT = document.querySelector('.create-text');
-const NOTIFICATIONS = document.querySelector('.notifications');
-const NOTIFICATIONS_TEXT = document.querySelector('.notifications-text');
+const BTN_SEARCH = document.querySelector(".btn-search");
+const BTN_SEARCH_POPUP = document.querySelector(".search");
+const MICROPHONE = document.querySelector(".microphone");
+const SEARCH_WITH_VOICE = document.querySelector(".search-with-voice");
+const CREATE = document.querySelector(".create");
+const CREATE_TEXT = document.querySelector(".create-text");
+const NOTIFICATIONS = document.querySelector(".notifications");
+const NOTIFICATIONS_TEXT = document.querySelector(".notifications-text");
 
-CREATE.addEventListener('mouseover', () => {
-    CREATE_TEXT.style.visibility = 'visible';
-})
-CREATE.addEventListener('mouseout', () => {
-    CREATE_TEXT.style.visibility = 'hidden';
-})
+MICROPHONE.addEventListener("mouseover", () => {
+  SEARCH_WITH_VOICE.style.visibility = "visible";
+});
+MICROPHONE.addEventListener("mouseout", () => {
+  SEARCH_WITH_VOICE.style.visibility = "hidden";
+});
 
+BTN_SEARCH.addEventListener("mouseover", () => {
+  BTN_SEARCH_POPUP.style.visibility = "visible";
+});
+BTN_SEARCH.addEventListener("mouseout", () => {
+  BTN_SEARCH_POPUP.style.visibility = "hidden";
+});
 
-NOTIFICATIONS.addEventListener('mouseover', () => {
-    NOTIFICATIONS_TEXT.style.visibility = 'visible';
-})
-NOTIFICATIONS.addEventListener('mouseout', () => {
-    NOTIFICATIONS_TEXT.style.visibility = 'hidden';
-})
+CREATE.addEventListener("mouseover", () => {
+  CREATE_TEXT.style.visibility = "visible";
+});
+CREATE.addEventListener("mouseout", () => {
+  CREATE_TEXT.style.visibility = "hidden";
+});
+
+NOTIFICATIONS.addEventListener("mouseover", () => {
+  NOTIFICATIONS_TEXT.style.visibility = "visible";
+});
+NOTIFICATIONS.addEventListener("mouseout", () => {
+  NOTIFICATIONS_TEXT.style.visibility = "hidden";
+});
